758c27febbc03c17eeb3bcde88174f319c059fb5
samtietze <sam.tietze@spr.com>
2018-04-10 17:03:52 -0500

    clean up and revise layout files to properly display History list items

--- app/src/main/java/com/spr/wikipediademo/activities/MainActivity.kt
+++ app/src/main/java/com/spr/wikipediademo/activities/MainActivity.kt
@@ -42,6 +42,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar)
+
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
         val transaction = supportFragmentManager.beginTransaction()
